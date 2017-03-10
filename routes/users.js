beb1c2d4438dfa60ca7043e79eca14f3d6a03ae4
awaisabir <awaisabirqureshi@gmail.com>
2017-03-09 21:46:51 -0500

    Added Schema, methods to add

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 
 // Register
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
 
-	res.send('Register Route');
+	let newUser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	User.addUser(newUser, (err, user) => {
+
+		if(err)
+			res.json({success: false, message: "Failed to register user"});
+		else {
+
+			res.json({success: true, message: "User registerd"});	
+		}
+	});
+
 });
 
 // Authenticate
@@ -18,12 +37,6 @@ router.post('/authenticate', (req, res, next) => {
 router.get('/profile', (req, res, next) => {
 
 	res.send('Profile Route');
-});
-
-// Validate
-router.get('/validate', (req, res, next) => {
-
-	res.send('Validate Route');
 });
 
 // Other Routes
