c4a94f1066c5dce67db68822dc9b51dea1d4728b
awaisabir <awaisabirqureshi@gmail.com>
2017-03-16 01:00:33 -0400

    Made changes with the flash messages

--- frontend-src/src/app/components/register/register.component.ts
+++ frontend-src/src/app/components/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+/*
+  Anytime we use a service, we need to inject it into the constructor of the
+  component.
+*/
 
 @Component({
   selector: 'app-register',
@@ -7,14 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  // variables from the form
   name: String;
   username: String;
   email: String;
   password: String;
-  
-  constructor() { }
+
+  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  onRegisterSubmit() {
+    let user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // validation for email
+    if(!this.validateService.validateEmail(this.email)) {
+      // show the flash message
+      this.flashMessage.show("Please input a correct email!", {cssClass: 'alert-danger container', timeout: 2000});
+    }
+  }
 }
